6693cd900d05537a2b94b5dd4861d0f59698324f
pankajbhamoriya <38315265+pankajbhamoriya@users.noreply.github.com>
2020-02-18 13:03:18 +0530

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,9 +16,9 @@ this.usersList=data["results"]
 })}
 load()
 {
-this.http.get('https://randomuser.me/api/?results=${this.firstName}').subscribe(res=>{
+this.http.get(`https://randomuser.me/api/?results=${this.firstName}`).subscribe(res=>{
 this.users=res["results"];
-  console.log('https://randomuser.me/api/?results=${this.firstName}');
+  console.log(`https://randomuser.me/api/?results=${this.firstName}`);
   console.log(this.firstName);
 })
 }
